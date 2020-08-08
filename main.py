08e78fafa40204ae2c9c70e7fbd2313236631462
TonyKutunio <55698444+TonyKutunio@users.noreply.github.com>
2020-08-08 12:40:32 +0100

    Added logger and argparse

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import vk_api
 from dotenv import load_dotenv
 import telegram
 import requests
+import argparse
+import logging
+
+
+logger = logging.getLogger("Facebook Requests Response")
 
 
 def post_to_vk(picture_path, text):
@@ -18,48 +23,71 @@ def post_to_vk(picture_path, text):
     vk = vk_session.get_api()
     attachments = f'photo{vk_user_id}_{picture_id}'
     vk.wall.post(message=text,
-                 owner_id=-vk_group_id,
+                 owner_id= -vk_group_id,
                  attachments=attachments)
 
 
 def post_to_fb(picture_path, text):
     url = f"https://graph.facebook.com/v2.11/{fb_group_id}/photos"
-    files = {'upload_files': open(picture_path, 'rb')}
+
     data = {'caption': text,
             'access_token': fb_token,
             }
-    response = requests.post(url, files=files, data=data)
-    print('Facebook response: ' + response.text)
-    response.raise_for_status()
+    with open(picture_path, 'rb') as file:
+        files = {'upload_files': file}
+        response = requests.post(url, files=files, data=data)
+        logger.debug(response.text)
+        response.raise_for_status()
 
 
 def post_to_telegram(picture_path, text):
     bot = telegram.Bot(token=tg_bot_token)
-    bot.send_photo(chat_id=tg_chat_id,
+    bot.send_photo(chat_id=tg_chanel_name,
                    photo=open(picture_path, 'rb'))
-    bot.send_message(chat_id=tg_chat_id, text=text)
+    bot.send_message(chat_id=tg_chanel_name, text=text)
 
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser(
+        description='Takes two arguments.'
+                    '1. Text'
+                    '2. File Path')
+
+    parser.add_argument('file_path',
+                        type=str,
+                        help='File path of picture to be posted')
+
+    parser.add_argument('text',
+                        type=str,
+                        help='Text to be posted')
+    return parser
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARNING)
+    logger.setLevel(logging.DEBUG)
     load_dotenv()
     vk_login = os.getenv('VK_LOGIN')
     vk_password = os.getenv('VK_PASSWORD')
     vk_access_token = os.getenv('VK_ACCESS_TOKEN')
-    vk_group_id = 'GroupID'
-    vk_user_id = 'Your_User_ID'
+    vk_group_id = int(os.getenv('VK_GROUP_ID'))
+    vk_user_id = os.getenv('VK_USER_ID')
 
     fb_token = os.getenv('FACEBOOK_TOKEN')
-    fb_group_id = 'Your_facebook_group_id'
+    fb_group_id = int(os.getenv('FB_GROUP_ID'))
 
     tg_bot_token = os.getenv('TG_BOT_TOKEN')
-    tg_chat_id = '@telegram_chanel_name'
+    tg_chanel_name = os.getenv('TG_CHANNEL_NAME')
 
-    picture_path = 'picture_path'
-    text = 'any text'
+
+    parser = get_argument_parser()
+    args = parser.parse_args()
+    picture_path = args.file_path
+    text = args.text
 
     post_to_vk(picture_path, text)
     post_to_fb(picture_path, text)
     post_to_telegram(picture_path, text)
+
 
 
